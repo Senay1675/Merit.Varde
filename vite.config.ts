6ec1857c13b282f7a1493210a815576a1e2c4024
Senay1675 <senay_93@hotmail.com>
2025-04-10 17:13:01 +0200

    lyckades deploya

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Merit.Varde', // Uppdatera detta
+  base: '/Merit.Varde/', // Uppdatera detta
 
 })
